12ac2110efa48b80acd284dd663a7d0aa844872e
NagmaKhattar <nagmakhattar@gmail.com>
2024-09-18 12:46:14 -0400

    added a branch

--- src/week3wednesday/Week3Wednesday.java
+++ src/week3wednesday/Week3Wednesday.java
@@ -20,6 +20,7 @@ public class Week3Wednesday {
     //comment
     //illustrating pull
     //illustrating fetch and merge
+    //branch added
     public static void main(String[] args) {
         // TODO code application logic here
     }
